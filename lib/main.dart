c8e23c082ea326a59d9e20e336f514a58b959ef0
Your Name <you@example.com>
2026-02-02 21:12:57 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(ComicWorld());
+}
+
+class ComicWorld extends StatelessWidget {
+  const ComicWorld({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
